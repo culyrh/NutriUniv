336d2e6b95b94dfb7e4e5e5cc61a227be8da93bb
ubuntu <ict_701@naver.com>
2026-05-09 15:11:59 +0900

    fix: 검색 request에 @Setter 추가

--- src/main/java/com/example/nutriuniv/domain/product/dto/AdminProductSearchRequest.java
+++ src/main/java/com/example/nutriuniv/domain/product/dto/AdminProductSearchRequest.java
@@ -2,8 +2,10 @@ package com.example.nutriuniv.domain.product.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class AdminProductSearchRequest {
 
